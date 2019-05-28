85407efdd513c5375aca03e893c5b3acb1132366
lamj3612 <50212370+lamj3612@users.noreply.github.com>
2019-05-28 08:40:24 -0700

    new changes

--- dev/cdemo/arrays.c
+++ dev/cdemo/arrays.c
@@ -3,21 +3,21 @@
 void arrayAdd();
 void main()
 {
-  int arr[100];
-  for (int a = 0; a < 100; a++)
-    {
+int arr[100];
+for (int a = 0; a < 100; a++)
+{
     arr[a] = a * a;
     printf("index = %d, value = %d\n", arr[a]);
-    } 
+} 
     printf("\n");
     arrayAdd(arr, 100, 35);
 }
 
 void arrayAdd(int* arr, int f, int n)
 {
-  for(int num = 0; num < f; num++)
-  {
+for(int num = 0; num < f; num++)
+{
     arr[num] = arr[num] + n;
     printf("index = %d, value = %d\n",num, arr[num]);
-  }
+}
 }
